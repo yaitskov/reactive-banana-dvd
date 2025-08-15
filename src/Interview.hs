c4117664a1739bd1a1efaddee5b7a5aa969b72e9
Daniil Iaitskov <dyaitskov@gmail.com>
2025-08-15 12:16:50 -0800

    master update message row by timer

--- src/Interview.hs
+++ src/Interview.hs
@@ -2,21 +2,33 @@
 {-# LANGUAGE ApplicativeDo #-}
 module Interview where
 
+import Control.Concurrent
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 import Termbox.Banana as TB
 
 import Relude
 
+
+
 main :: IO ()
 main = TB.run program >>= \case
   Left e -> fail $ show e
   Right _ -> pure ()
 
+
 program :: Inputs -> MomentIO (Outputs Key)
 program i = do
+  (tickEvent, fireTickEvent) <- newEvent
+  let ticker = do
+        fireTickEvent ()
+        threadDelay (10 ^ (5 :: Int))
+        ticker
+  liftIO $ forkIO ticker
+
+  xPos :: Behavior Int <- accumB 0 ((+1) <$ tickEvent)
   effectiveSize <- stepper (initialSize i) (resizes i)
-  let firstColumn :: Behavior (Int, Int) = (\es -> ((width es - txtLen) `div` 2, height es `div` 2)) <$> effectiveSize
+  let firstColumn :: Behavior (Int, Int) = (\es x -> ((width es - txtLen) `div` 2, x `mod` height es)) <$> effectiveSize <*> xPos
   pure $ Outputs
     { scene =
         fmap s firstColumn
