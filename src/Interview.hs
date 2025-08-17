d8122b04e95436f03d2db6be416bec3f8b874909
Daniil Iaitskov <dyaitskov@gmail.com>
2025-08-17 12:16:02 -0800

    master comment attempts to change

--- src/Interview.hs
+++ src/Interview.hs
@@ -19,14 +19,23 @@ main = TB.run program >>= \case
 
 program :: Inputs -> MomentIO (Outputs Key)
 program i = do
-  (tickEvent, fireTickEvent) <- newEvent
+  (tickEvent :: Event (), fireTickEvent) <- newEvent
   let ticker = do
         fireTickEvent ()
-        threadDelay (10 ^ (5 :: Int))
+        threadDelay (10 ^ (6 :: Int))
         ticker
   liftIO $ forkIO ticker
-
-  xPos :: Behavior Int <- accumB 0 ((+1) <$ tickEvent)
+  -- (<@>) :: Behavior (a -> b) -> Event a -> Event b
+  -- (<@) :: Behavior b -> Event a -> Event b
+  -- stepper :: MonadMoment m => a -> Event a -> m (Behavior a)
+  -- whenE :: Behavior Bool -> Event a -> Event a
+  -- accumB :: MonadMoment m => a -> Event (a -> a) -> m (Behavior a)
+  -- filterE :: (a -> Bool) -> Event a -> Event a
+  -- let speed :: Event (Int -> Int) = pure (\case KeyArrowUp -> (+1); KeyArrowDown -> flip (-) 1; _ -> (+0)) <@> keys i
+  -- speed' :: Behavior Int <- stepper 1 ((fmap (flip (-) 1) speed) <@ filterE (==KeyArrowDown) (keys i))
+  -- let plusSpeed :: Behavior (Int -> Int) =  (+) <$> speed
+  -- let plusSpeed' :: Event (Int -> Int) = speed <@ tickEvent
+  xPos :: Behavior Int <- accumB 0 ( (+1) <$ tickEvent)
   effectiveSize <- stepper (initialSize i) (resizes i)
   let firstColumn :: Behavior (Int, Int) = (\es x -> ((width es - txtLen) `div` 2, x `mod` height es)) <$> effectiveSize <*> xPos
   pure $ Outputs
